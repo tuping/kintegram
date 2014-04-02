39c3e45ac41e04cd469b73c738b02babaf3cbf5c
Prelang Builder <builder@prelang.com>
2014-04-02 13:46:58 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kintegram::Application.config.secret_key_base = '2805b4264e076649a12682c0c6ea522f2cf503a298da6922bb3e707e5bf36cd890fcd0ba55766f93c712899579926535e6dc1d74ec10c1884631a0abbf8db49e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kintegram::Application.config.session_store :cookie_store, key: '_kintegram_session'
